bb9ca6103d0dea6653cee2c953b203645416f7a9
aryan-negi151 <lnegi8596@gmail.com>
2025-11-11 21:53:24 +0530

    palindrome

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,9 @@
 // write a program to find if a number is palindrome or not
+
+#include<stdio.h>
+int main(){
+//please enter a number
+//scan it
+// check if number is plaindrome or not, return true /false.
+
+}
